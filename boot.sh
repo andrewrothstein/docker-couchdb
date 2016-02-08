e6e4164de5756a8a25ec9bb2301e8629749c7c8d
Andrew Rothstein <andrew.rothstein@gmail.com>
2016-02-08 10:33:06 -0500

    include the link to couchdb-lucene

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker run -d -p 5985:5985 --name couchdb-lucene greenlizard/couchdb-lucene
-docker run -d -p 5984:5984 --name couchdb andrewrothstein/couchdb
+docker run -d -p 5984:5984 --name couchdb --link couchdb-lucene:couchdb-lucene andrewrothstein/couchdb
 
